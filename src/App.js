aa905478a4ee43656d584bc15711e5a8b7bd9974
steroyle <steroyle@gmail.com>
2021-08-05 15:50:43 +0100

    added single line es-lint disable

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
       setIsLoading(false)
     }
     fetchData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const loadingPokemon = async (data) => {
