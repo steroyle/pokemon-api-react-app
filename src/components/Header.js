d48840914598c90423b0a5ba05b4a838a02f324a
steroyle <steroyle@gmail.com>
2021-08-05 16:01:25 +0100

    added repo link

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header>
       <h1 className="title"><a href="/">Pokemon API React App</a></h1>
-      <p>by Ste Royle</p>
+      <p className="github"><a href="https://github.com/steroyle/pokemon-api-react-app">View on GitHub</a></p>
     </header>
   )
 }
